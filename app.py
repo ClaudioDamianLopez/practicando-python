8020fbc9d0ef95cb660fca3f68b2e659feec93ea
Claudio-lopez <claudio.damian.lopez@gmail.com>
2022-08-10 21:23:02 -0300

    add admin

--- app.py
+++ app.py
@@ -18,5 +18,10 @@ def nosotros():
     return render_template('sitio/nosotros.html')
 
 
+@app.route('/admin')
+def admin_index():
+    return render_template('admin/index.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
